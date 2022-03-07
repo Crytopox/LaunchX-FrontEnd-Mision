d5fd7c14709b393a4eaa8f55e8a3030000fe6afb
Crytopox <crytopox@gmail.com>
2022-03-06 22:21:20 -0600

    Practica Modulo HTML

---
+++ 02 - HTML/LaunchX-HTML/LaunchX-HTML/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaunchX_HTML.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            return View();
+        }
+
+        public ActionResult Ordenar()
+        {
+            return View();
+        }
+
+        public ActionResult Pedidos()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contactanos con los siguientes metodos:";
+
+            return View();
+        }
+    }
+}
---
+++ 02 - HTML/LaunchX-HTML/LaunchX-HTML/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LaunchX_HTML.MvcApplication" Language="C#" %>
